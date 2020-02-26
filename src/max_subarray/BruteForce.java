cfe76cb6f77bbda6dbbe65cae3f93a0d98711177
180138Ga <grace.m.arnold@outlook.com>
2020-02-26 14:11:35 -0600

    Cleaning up & adding comments to BruteForce

--- src/max_subarray/BruteForce.java
+++ src/max_subarray/BruteForce.java
@@ -3,71 +3,74 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BruteForce {
-	static ArrayList<Integer> findMaxSubarray(ArrayList<Integer> a) {
-		int sum = 0;
-		int max = 0;
-		ArrayList<Integer> maxArray = new ArrayList<Integer>();
+	/**
+	 * findMaxSubarray uses the brute force method to find the maximum sum of integers in a given integer ArrayList
+	 * @param input is the input ArrayList
+	 * @return an ArrayList with the indices of the maximum subarray and the sum of their values.
+	 */
+	static ArrayList<Integer> findMaxSubarray(ArrayList<Integer> input) {
+		// max is the maximum sum of all subarrays
+		// it is set to -100, assuming none of the subarrays sum up to less than -100
+		int max = -100;
+		// answer is the ArrayList which includes the indices of the maximum subarray and the sum of their values
 		ArrayList<Integer> answer = new ArrayList<Integer>();
 		
-		for(int i = 0; i < a.size(); i++) {
-			//System.out.println("I: " + i);
-			
+		// This for loop goes through each element in the input ArrayList
+		for(int i = 0; i < input.size(); i++) {
+			// sum is the sum of the current subarray
+			int sum = 0;
+			// array is the ArrayList of values in the current subarray
 			ArrayList<Integer> array = new ArrayList<Integer>();
-			//array.add(a.get(i));
+			// indices is the ArrayList of indices in the current subarray
+			ArrayList<Integer> indices = new ArrayList<Integer>();
 			
-			//sum = sum + a.get(i);
-			
-			for(int j = i; j < a.size(); j++) {
-				
-				//System.out.println("j: " + j);
-				array.add(a.get(j));
+			// This for loop makes each subarray for each element in the input ArrayList
+			for(int j = i; j < input.size(); j++) {
+				// The current value is added to array
+				array.add(input.get(j));
+				// The current index is added to indices
+				indices.add(j);
+				// The sum of the subarray is calculated
 				sum = sum(array);
+				
+				// If the sum of this array is greater than the max some of arrays,
 				if(sum > max) {
-					//System.out.println("Max Before: " + max);
+					// The current sum becomes the new max
 					max = sum;
-					//System.out.println("Max After: " + max);
-					maxArray = new ArrayList<Integer>(array);
-					answer = new ArrayList<Integer>(maxArray);
+					// And the indices and sum are added to the answer ArrayList
+					answer = new ArrayList<Integer>(indices);
 					answer.add(max);
-					sum = 0;
 				}
 			}
 		}
 		return answer;
 	}
+	/**
+	 * A helper sum method to sum all the values in an array
+	 * @param a is the input ArrayList
+	 * @return the sum of the given ArrayList
+	 */
 	static int sum(ArrayList<Integer> a) {
 		int sum = 0;
-		for(int i = 0; i<a.size();i++) {
+		for(int i = 0; i < a.size();i++) {
 			sum = sum + a.get(i);
 		}
 		return sum;
 	}
 	public static void main(String[] args) {
-		BruteForce bf = new BruteForce();
 		Scanner scan = new Scanner(System.in);
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		//System.out.println("===========================\nBefore Method\n===========================\n");
-		
-		System.out.println("How many do numbers do you want to insert?");
+		ArrayList<Integer> input = new ArrayList<Integer>();		
+		System.out.println("How many do numbers do you want to sort?");
 		int n = scan.nextInt();
-		for(int i = 0; i<n; i++) {
-			System.out.println("Insert a number: ");
+		for(int i = 0; i < n; i++) {
+			System.out.println("Insert the number at index " + i + " : ");
 			int j = scan.nextInt();
-			al.add(j);
+			input.add(j);
 		}
-		
-		//System.out.println("===========================\nMethod\n===========================\n");
-		ArrayList<Integer> al2 = bf.findMaxSubarray(al);
-		
-		//System.out.println("===========================\nAfter Method\n===========================\n");
-		for(int i = 0; i<al2.size();i++) {
-			if(i == al2.size()-1) {
-				System.out.println("Sum: " + al2.get(i));
-				
-			}else {
-				System.out.println("Index: " + al2.get(i));
-			}
-		}
+		ArrayList<Integer> arr = BruteForce.findMaxSubarray(input);
+		for(int i = 0; i < arr.size()-1;i++) 
+			System.out.println("Index: " + arr.get(i));
+		System.out.println("Sum: " + arr.get(arr.size()-1));
 	}
 
 }
